0aeb11b9743ec01527b7e1267a151c7fcd102031
quyetcodeweb <minh.quyet213@gmail.com>
2024-05-13 01:36:45 +0700

    Add files via upload
    create

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Loan';
+const giftUrl = 'a';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = '<3 For you <3';
+const contentCard = 'Chúc Loan có ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay cả những ngày sau.! <br> -Quyetbuonngu-';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
